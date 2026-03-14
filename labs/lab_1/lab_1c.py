817320b20a619e7ae7ac91536c89a388022d4b5d
joshs47 <joshuashi47@gmail.com>
2026-03-14 17:08:35 -0400

    test5

--- labs/lab_1/lab_1c.py
+++ labs/lab_1/lab_1c.py
@@ -21,12 +21,12 @@ def max_subarray_sum(nums: list[int]) -> int:
     """
 
     max_current = max_global = nums[0]
-    
+
     for num in nums[1:]:
         max_current = max(num, max_current + num)
         if max_current > max_global:
             max_global = max_current
-            
+
     return max_global
 
 # Example usage:
