91f6af963372064ec66089542e43817c987d89c5
joshs47 <joshuashi47@gmail.com>
2026-03-14 12:55:27 -0400

    test3

--- tests/test_max_subarray.py
+++ tests/test_max_subarray.py
@@ -2,4 +2,4 @@ from labs.lab_1.lab_1c import max_subarray_sum
 
 def test_example_case():
     nums = [-2,1,-3,4,-1,2,1,-5,4]
-    assert max_subarray_sum(nums) == 4
+    assert max_subarray_sum(nums) == 6
